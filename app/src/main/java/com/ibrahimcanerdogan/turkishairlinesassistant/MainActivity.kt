d3db94135db361efa065a2598b99d72f2078ff74
ibrahimcanerdogan <i.erdogan@ebebek.com>
2023-12-07 23:52:47 +0300

    Splash Screen designed.

--- app/src/main/java/com/ibrahimcanerdogan/turkishairlinesassistant/MainActivity.kt
+++ app/src/main/java/com/ibrahimcanerdogan/turkishairlinesassistant/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ibrahimcanerdogan.turkishairlinesassistant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import com.ibrahimcanerdogan.turkishairlinesassistant.databinding.ActivityMainBinding
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
