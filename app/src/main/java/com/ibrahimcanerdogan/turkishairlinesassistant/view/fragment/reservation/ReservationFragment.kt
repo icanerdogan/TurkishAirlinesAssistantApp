76af3f73fb667ff11a3fc0ff26984ebe13ce0cdd
ibrahimcanerdogan <i.erdogan@ebebek.com>
2023-12-17 18:45:23 +0300

    Reservation completed.

--- app/src/main/java/com/ibrahimcanerdogan/turkishairlinesassistant/view/fragment/reservation/ReservationFragment.kt
+++ app/src/main/java/com/ibrahimcanerdogan/turkishairlinesassistant/view/fragment/reservation/ReservationFragment.kt
@@ -1,18 +1,19 @@
-package com.ibrahimcanerdogan.turkishairlinesassistant.view.fragment
+package com.ibrahimcanerdogan.turkishairlinesassistant.view.fragment.reservation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.tabs.TabLayoutMediator
 import com.ibrahimcanerdogan.turkishairlinesassistant.databinding.FragmentReservationBinding
 import com.ibrahimcanerdogan.turkishairlinesassistant.model.reservation.request.international.ReservationInternationalRequestHeader
 import com.ibrahimcanerdogan.turkishairlinesassistant.model.reservation.request.international.ReservationInternationalRequest
 import com.ibrahimcanerdogan.turkishairlinesassistant.model.reservation.request.international.ReservationInternationalOTARequest
 import com.ibrahimcanerdogan.turkishairlinesassistant.model.reservation.request.international.reservationInternationalToJsonObject
+import com.ibrahimcanerdogan.turkishairlinesassistant.view.adapter.reservation.ReservationViewPagerAdapter
 import com.ibrahimcanerdogan.turkishairlinesassistant.view.viewmodel.reservation.ReservationViewModel
 import com.ibrahimcanerdogan.turkishairlinesassistant.view.viewmodel.reservation.ReservationViewModelFactory
 import dagger.hilt.android.AndroidEntryPoint
@@ -36,29 +37,22 @@ class ReservationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentReservationBinding.inflate(inflater, container, false)
+
+        binding.apply {
+            viewPager.adapter = ReservationViewPagerAdapter(requireActivity())
+            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+                when(position) {
+                    0 -> {
+                        tab.text = "Domestic Flights"
+                    }
+                    1 -> tab.text = "International Flights"
+                }
+            }.attach()
+        }
+
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        viewModel.getReservationDetailData(ReservationInternationalRequest(
-            requestHeader = ReservationInternationalRequestHeader(
-                requestHeaderAirlineCode = "TK"
-            ),
-            reservationInternationalOTARequest = ReservationInternationalOTARequest(
-                otaRequestUniqueId = "TT8VN8",
-                otaRequestSurname = "CELIKTAS"
-            )
-        ).reservationInternationalToJsonObject())
-
-        viewModel.reservationData.observe(viewLifecycleOwner) {
-            it?.let {
-                println(it.message)
-                Toast.makeText(requireContext(), it.message.description, Toast.LENGTH_LONG).show()
-            }
-        }
-    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
---
+++ app/src/main/java/com/ibrahimcanerdogan/turkishairlinesassistant/view/fragment/reservation/international/ReservationInternationalFragment.kt
@@ -0,0 +1,99 @@
+package com.ibrahimcanerdogan.turkishairlinesassistant.view.fragment.reservation.international
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.ibrahimcanerdogan.turkishairlinesassistant.R
+import com.ibrahimcanerdogan.turkishairlinesassistant.databinding.FragmentReservationInternationalBinding
+import com.ibrahimcanerdogan.turkishairlinesassistant.model.reservation.request.international.ReservationInternationalOTARequest
+import com.ibrahimcanerdogan.turkishairlinesassistant.model.reservation.request.international.ReservationInternationalRequest
+import com.ibrahimcanerdogan.turkishairlinesassistant.model.reservation.request.international.ReservationInternationalRequestHeader
+import com.ibrahimcanerdogan.turkishairlinesassistant.model.reservation.request.international.reservationInternationalToJsonObject
+import com.ibrahimcanerdogan.turkishairlinesassistant.model.reservation.response.ReservationResponse
+import com.ibrahimcanerdogan.turkishairlinesassistant.view.viewmodel.reservation.ReservationViewModel
+import com.ibrahimcanerdogan.turkishairlinesassistant.view.viewmodel.reservation.ReservationViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class ReservationInternationalFragment : Fragment() {
+
+    private var _binding: FragmentReservationInternationalBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel by lazy {
+        ViewModelProvider(this, factory).get(ReservationViewModel::class.java)
+    }
+
+    @Inject
+    lateinit var factory: ReservationViewModelFactory
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentReservationInternationalBinding.inflate(inflater, container, false)
+
+        handleObserve()
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            var airlinesCode = "TK"
+
+            radioButtonTurkishAirlines.setOnCheckedChangeListener { _, checkedId ->
+                if(checkedId) {
+                    airlinesCode = "TK"
+                    radioButtonAJet.isChecked = false
+                }
+            }
+            radioButtonAJet.setOnCheckedChangeListener { _, checkedId ->
+                if(checkedId) {
+                    airlinesCode = "AJ"
+                    radioButtonTurkishAirlines.isChecked = false
+                }
+            }
+
+            buttonReservationInternational.setOnClickListener {
+                viewModel.getReservationDetailData(ReservationInternationalRequest(
+                    requestHeader = ReservationInternationalRequestHeader(
+                        requestHeaderAirlineCode = airlinesCode
+                    ),
+                    reservationInternationalOTARequest = ReservationInternationalOTARequest(
+                        otaRequestUniqueId = binding.editTextReservationID.text.toString().uppercase(),
+                        otaRequestSurname = binding.editTextReservationSurname.text.toString().uppercase()
+                    )
+                ).reservationInternationalToJsonObject())
+            }
+        }
+    }
+
+    private fun handleObserve() {
+        viewModel.reservationData.observe(viewLifecycleOwner, ::getInternationalReservation)
+    }
+
+
+    private fun getInternationalReservation(reservationResponse: ReservationResponse?) {
+        reservationResponse?.let {
+            println(it.message)
+            Toast.makeText(requireContext(), it.message.description, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        const val TAG = "ReservationInternationalFragment"
+    }
+}
